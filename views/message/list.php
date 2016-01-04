6585aa2264e5f3f2cbbd530b06c9ca3ab079a877
Matthijs <matthijsamesz@hotmail.com>
2016-01-04 18:40:32 +0100

    Improved message css

--- views/message/list.php
+++ views/message/list.php
@@ -14,14 +14,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="message-view">
 
     <h1><?php Html::encode($this->title) ?></h1>
-
+	<ul>
     <?php
 	foreach($messages as $message) {
+		$c = "\"message\"";
+		if (Yii::$app->user->identity->id == $message->creator_id)
+			$c = "\"message my_message\"";
 		//echo HTML::encode(BackendUser::findOne($message->creator_id)->username . " says: <br>" . $message->content . "<br>posted at: " . $message->timestamp . "<br><br>");
-		echo BackendUser::findOne($message->creator_id)->username . " says: <br>" . $message->content . "<br>posted at: " . $message->timestamp . "<br><br>";
+		echo "<li class=" . $c . "><h5 class=\"username\">" . BackendUser::findOne($message->creator_id)->username . " says: </h5><div class=\"content\">" . $message->content . "</div><h6 class=\"timestamp\">posted at: " . $message->timestamp . "</h6></li>";
 	}
 
 
  ?>
-
+</ul>
 </div>
